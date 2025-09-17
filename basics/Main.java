f538fb32006633b538277cc43a5ff3fafa366611
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-09-18 00:10:14 +0300

    checkUp

---
+++ basics/Main.java
@@ -0,0 +1,12 @@
+class Main {
+  boolean result = true;
+  char capitalC = 'C';
+  byte b = 100;
+  short s = 10_000;
+  int i = 10_000_000;
+
+  // Integers' literal can be with specific prefix
+  int decimalValue = 26;
+  int hexadecimalValue = 0x1a;
+  int binaryValue = 0b11010;
+}
