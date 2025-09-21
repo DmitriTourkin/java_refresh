42c6beb3886b771db4a1d791c1e7cce24b385b7f
Dmitrii Tourkin <ddiimmuuzz@yandex.ru>
2025-09-22 00:07:06 +0300

    Switch Expression

---
+++ basics/SwitchDemo.java
@@ -0,0 +1,25 @@
+public class SwitchDemo {
+  public static void main(String[] args) {
+    int quarter = 3;
+
+    String quarterLabel = 
+    switch (quarter) {
+      case 0 -> "Q1-Winter";
+      case 1 -> "Q2-Spring";
+      case 2 -> "Q3 - Summer";
+      case 3 -> "Q3 - Summber";
+      default -> "Unknown quarter";
+    };
+
+    quarter = 0;
+
+    quarterLabel =
+    switch (quarter) {
+      case 0 -> {
+        System.out.println("Q1 - Winter");
+        yield "Q1 - Winter";
+      }
+      default -> "Unknown quarter";
+    };
+  }
+}
